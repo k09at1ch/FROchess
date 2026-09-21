cdf76826cbe51cdc1cca657338e44aefb70ce7d3
k09at1ch <newwerasik228@gmail.com>
2026-09-21 23:05:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
 import Home from './components/home/home.jsx'
 import Play from './components/play/play.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/play" element={<Play />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
